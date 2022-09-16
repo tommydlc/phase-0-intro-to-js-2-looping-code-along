c9a21b95d34aa93a381319f7dd5d6d64493ba763
Tommy De La Cruz <tommydlc08@gmail.com>
2022-09-15 22:42:30 -0400

    commit tree b391fd0d589d3e1184c330e8808d20ee7f3a9bef

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number) {
+     
+    while (number >= 0){
+        console.log(number--)
+    }
+
+}
